e1cede6eacd4201d663659265ca39218a4ddac90
Lacey49 <gridleykasey@gmail.com>
2026-09-10 11:33:42 -0700

    Load neighborhood rebuild optimizer pass

--- assets/js/optimizer.js
+++ assets/js/optimizer.js
@@ -7,6 +7,7 @@
     './optimizer/optimizer.js?v=10',
     './optimizer/performance.js?v=1',
     './optimizer/experimental.js?v=10',
+    './optimizer/neighborhood.js?v=1',
     './presets/current-presets.js?v=1',
     './site/keyboard.js?v=1'
   ];
