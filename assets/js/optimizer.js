9f0f9d472c5d47bffdcc28c302025fe9ab3f30ea
Lacey49 <gridleykasey@gmail.com>
2026-09-10 11:26:05 -0700

    Load optimizer performance helpers

--- assets/js/optimizer.js
+++ assets/js/optimizer.js
@@ -5,6 +5,7 @@
 
   const scripts = [
     './optimizer/optimizer.js?v=10',
+    './optimizer/performance.js?v=1',
     './optimizer/experimental.js?v=10',
     './presets/current-presets.js?v=1',
     './site/keyboard.js?v=1'
